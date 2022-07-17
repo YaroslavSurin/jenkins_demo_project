7ed2e7a0d384935f761552ed57380b0601e00859
Yaroslav Surin <allstarcream@gmail.com>
2022-07-17 11:07:31 -0400

    Project updated to be able to run test in Selenium Grid

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.205.20.39";
+                        String gridAddress = "52.23.201.172";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
